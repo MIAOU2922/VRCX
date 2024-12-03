2be87cce1d258f6bc5463e97597ac326f83796f9
kikookraft <kikookraft.teamcraft@gmail.com>
2024-12-03 14:46:29 +0100

    updated release repo

--- html/src/classes/vrcxUpdater.js
+++ html/src/classes/vrcxUpdater.js
@@ -22,14 +22,14 @@ export default class extends baseClass {
         branches: {
             Stable: {
                 name: 'Stable',
-                urlReleases: 'https://api0.vrcx.app/releases/stable',
-                urlLatest: 'https://api0.vrcx.app/releases/stable/latest'
-            },
-            Nightly: {
-                name: 'Nightly',
-                urlReleases: 'https://api0.vrcx.app/releases/nightly',
-                urlLatest: 'https://api0.vrcx.app/releases/nightly/latest'
+                urlReleases: 'https://api.github.com/repos/MIAOU2922/VRCX/releases',
+                urlLatest: 'https://api.github.com/repos/MIAOU2922/VRCX/releases/latest'
             }
+            // Nightly: {
+            //     name: 'Nightly',
+            //     urlReleases: 'https://api0.vrcx.app/releases/nightly',
+            //     urlLatest: 'https://api0.vrcx.app/releases/nightly/latest'
+            // }
         }
     };
 
